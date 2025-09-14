248c56555c1c95fd4004df5b101bff8926ee319f
LEDAT\ledat <ledat260701@gmail.com>
2025-09-14 10:19:55 +0700

    Database, Login, REgister

---
+++ features/auth/constants.ts
@@ -0,0 +1,2 @@
+export const AUTH_COOKIE_NAME = "cwa-task-manager-session";
+
--- features/auth/server/route.ts
+++ features/auth/server/route.ts
@@ -2,21 +2,52 @@ import { Hono } from "hono";
 
 import { zValidator } from "@hono/zod-validator";
 import { formLoginSchema, formSignUpSchema } from "../schema";
+import { createAdminClient } from "@/lib/appwrite";
+import { ID } from "node-appwrite";
+import { deleteCookie, setCookie } from "hono/cookie";
+import { AUTH_COOKIE_NAME } from "../constants";
+import { success } from "zod";
 
 const app = new Hono()
-  .post("/login", zValidator("json", formLoginSchema), (c) => {
+  .post("/login", zValidator("json", formLoginSchema), async (c) => {
     const { email, password } = c.req.valid("json");
 
-    console.log(email, password);
+    const { account } = await createAdminClient();
+    const session = await account.createEmailPasswordSession(email, password);
 
-    return c.json({ email, password });
+    setCookie(c, AUTH_COOKIE_NAME, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30, // 30 days
+    });
+
+    return c.json({ success: true });
   })
-  .post("/register", zValidator("json", formSignUpSchema), (c) => {
+  .post("/register", zValidator("json", formSignUpSchema), async (c) => {
     const { email, password, name } = c.req.valid("json");
 
-    console.log(email, password, name);
+    const { account } = await createAdminClient();
 
-    return c.json({ email, password, name });
+    const user = await account.create(ID.unique(), email, password, name);
+
+    const session = await account.createEmailPasswordSession(email, password);
+
+    setCookie(c, AUTH_COOKIE_NAME, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30, // 30 days
+    });
+
+    return c.json({ data: user });
+  })
+  .post("/logout", async (c) => {
+    deleteCookie(c, AUTH_COOKIE_NAME);
+
+    return c.json({ success: true });
   });
 
 export default app;
